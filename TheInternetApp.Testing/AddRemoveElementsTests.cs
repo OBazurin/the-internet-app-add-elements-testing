baa0aa886eab6bec9e52716e146f7bdd1e441f7b
Oleks Bazurin <a.bazurin@gmail.com>
2023-03-21 16:12:56 -0600

    AddRemoveElementsTests.AddRemoveComponentsTest add verification that there are no buttons before adding

--- TheInternetApp.Testing/AddRemoveElementsTests.cs
+++ TheInternetApp.Testing/AddRemoveElementsTests.cs
@@ -5,6 +5,7 @@ public class AddRemoveElementsTests : BaseTest
     [Test]
     public void AddRemoveComponentsTest([Random(1,10,1)]int numberOfButtonsToAdd)
     {
+        AddRemoveElementsPage.GetAmountOfCreatedButtonsOnPage().Should().Be(0);
         AddRemoveElementsPage.AddButtons(numberOfButtonsToAdd);
         AddRemoveElementsPage.GetAmountOfCreatedButtonsOnPage().Should().Be(numberOfButtonsToAdd);
     }
