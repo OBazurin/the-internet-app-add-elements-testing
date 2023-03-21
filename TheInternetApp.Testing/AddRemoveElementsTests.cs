ae1119bd0807376091a50303042b5d98e0ff3709
Oleks Bazurin <a.bazurin@gmail.com>
2023-03-21 16:14:04 -0600

    AddRemoveElementsTests.AddRemoveComponentsTest renamed to AddRemoveElementsTests.AddButtonsTest

--- TheInternetApp.Testing/AddRemoveElementsTests.cs
+++ TheInternetApp.Testing/AddRemoveElementsTests.cs
@@ -3,7 +3,7 @@ namespace TheInternetApp.Testing;
 public class AddRemoveElementsTests : BaseTest
 {
     [Test]
-    public void AddRemoveComponentsTest([Random(1,10,1)]int numberOfButtonsToAdd)
+    public void AddButtonsTest([Random(1,10,1)]int numberOfButtonsToAdd)
     {
         AddRemoveElementsPage.GetAmountOfCreatedButtonsOnPage().Should().Be(0);
         AddRemoveElementsPage.AddButtons(numberOfButtonsToAdd);
